6c4e497fbdd426c9fe9b8b7f07afb7b6383cf915
jatinder kaur <jatinderraghubans@gmail.com>
2018-03-07 17:28:23 -0800

    Add project files.

---
+++ math-app-android/GameScreenAS.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace math_app_android
+{
+    [Activity(Label = "GameScreenAS")]
+    public class GameScreenAS : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.GameScreenAS);
+
+            // Create your application here
+            //for adding value to the operator
+            TextView textView = FindViewById<TextView>(Resource.Id.Num1);
+            TextView textNum = FindViewById<TextView>(Resource.Id.Num2);
+            Random r = new Random();
+           textView.Text= r.Next(0, 100).ToString();
+            textNum.Text = r.Next(0, 100).ToString();
+
+            //back button
+            Button button = FindViewById<Button>(Resource.Id.butBack);
+            button.Click += delegate
+            {
+                StartActivity(typeof(MainActivity)); 
+
+            };
+
+        }
+        private void Numbers()
+        {
+            Random r = new Random();
+            r.Next(0, 100);
+        }
+    }
+}
---
+++ math-app-android/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace math_app_android
+{
+    [Activity(Label = "math_app_android", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private static readonly long COUNTDOWN_IN_MILLS = 60000;
+        ////private CountDownTimer countdowntimer;
+        private long timeLeftInMills;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            //to set different screens 
+            Button button = FindViewById<Button>(Resource.Id.butAS);
+
+            button.Click += delegate
+            {
+                StartActivity(typeof(GameScreenAS));
+              
+            };
+
+
+        }
+        //private void StartDownCount()
+        //{
+        //    countdowntimer = new CountDownTimer(timeLeftInMills, 1000);
+        //}
+
+        //private void StartDownCount()
+        //{
+
+        //}
+        //protected virtual void onTick(long millsUntilFinished)
+        //{
+        //    TextView textView = FindViewById<TextView>(Resource.Id.textScore);
+        //    timeLeftInMills = millsUntilFinished;
+        //    if (COUNTDOWN_IN_MILLS > 0)
+        //    {
+        //        timeLeftInMills = timeLeftInMills - 1;
+
+        //        textView.Text = timeLeftInMills + " seconds";
+
+        //    }
+        //    else
+        //    {
+        //        textView.Text = "No time left";
+        //    }
+        //}
+
+
+    }
+
+    }
+
+
+
+
